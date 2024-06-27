b66fd301245f9a50688d7f829f9bd3c581311e00
Maxime Stapelle <maxime.stapelle@gmail.com>
2024-06-27 10:07:36 +0200

    Performance issue since function pointers: N times a loop on dimensions that can't be unrolled. Causes mutliple calls to fprintf. Created function for print in files with explicit switch on dimension.

--- src/Attractor.c
+++ src/Attractor.c
@@ -1,4 +1,5 @@
 #include "Dynamics.h"
+#include "Utilities.h"
 
 /*
 	The attractor function will compute the state of the selected dynamical system,
@@ -14,22 +15,15 @@ void attractor() {
 
 	for (size_t d = 0; d < dimension; d++) {
 		state[d] = (double) userMapValues.IC[d];
-		fprintf(fp, "%.4lf", state[d]);
-		if (d < dimension - 1) fprintf(fp, " ");
 	}
-	fprintf(fp, "\n");
+	fillFile(fp, NULL, state);
 
 
 	/*	EXACTLY THE SAME for flows and maps.  */
 	for (size_t n = 1; n <= N; n++) {
 		dynamics(state);
-		for (size_t d = 0; d < dimension; d++) {
-			fprintf(fp, "%.4lf", state[d]);
-			if (d < dimension - 1) fprintf(fp, " ");
-		}
-		fprintf(fp, "\n");
+		fillFile(fp, NULL, state);
 	}
-
-	printf("Data file created: '%s'.\n", dataFile);
 	fclose(fp);
+	printf("Data file created: '%s'.\n", dataFile);
 }
